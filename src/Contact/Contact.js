e386a2c41482030e8190ba9694d087c5360b3e98
Cédric Bazureau <cbazureau@gmail.com>
2017-10-17 23:12:41 +0200

    feat(addStyle): add style

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,7 @@ import './Contact.css';
 const Contact = () => (
   <div className="Contact">
     <span className="Contact__text">
-      Si vous avez une question ou une proposition d'emploi (de préférence sur Lyon) vous pouvez
+      Si vous avez une question ou une proposition d'emploi vous pouvez
       me contacter via mon profil linkedin.
     </span>
     <a href="https://www.linkedin.com/in/cbazureau" className="Contact__CTA">Contactez-moi !</a>
