ac6eedb6e42fb046cbb369c703c3121ffd00fb81
Cédric Bazureau <cbazureau@gmail.com>
2018-10-28 11:43:12 +0100

    feat(wording): fix stack

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import Job from './Job/Job';
-import Expert from './Expert/Expert';
-import Title from './Title/Title';
-import Contact from './Contact/Contact';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Job from "./Job/Job";
+import Expert from "./Expert/Expert";
+import Title from "./Title/Title";
+import Contact from "./Contact/Contact";
+import "./App.css";
 
 class App extends Component {
-
-  handleSectionChange = (section) => () => {
-    const node = ReactDOM.findDOMNode(this.refs[section])
-    if (window.getComputedStyle(node.parentNode).position === 'relative') {
+  handleSectionChange = section => () => {
+    const node = ReactDOM.findDOMNode(this.refs[section]);
+    if (window.getComputedStyle(node.parentNode).position === "relative") {
       window.scrollTo(0, node.parentNode.offsetTop + node.offsetTop);
     } else {
       window.scrollTo(0, node.offsetTop);
     }
-  }
+  };
 
   render() {
     return (
@@ -35,20 +34,24 @@ class App extends Component {
                 url="https://group.renault.com/en/"
                 description="Lead Tech des équipes (4 équipes, + de 15 developpeurs)
                   dans le cadre de la refonte des sites du groupe Renault (Renault, Nissan, Alpine)
-                  qui seront déployés dans plus de 50 pays." >
-                  <ul>
-                    <li>Stack ReactJs :
-                      <ul>
-                        <li>SSR, Chunking, ...</li>
-                        <li>Redux, Webpack, Babel, Thunk, Axios, ...</li>
-                        <li>Jest, Enzyme, Cucumber, BrowserStack, ...</li>
-                        <li>Styleguidist, Eslint, Flow, ...</li>
-                      </ul>
-                    </li>
-                    <li>Gitlab, Gitlab-Ci</li>
-                    <li>Nginx, AEM, Controller en Java</li>
-                    <li>Hébergement AWS : Cloudfront, Docker/ECS</li>
-                  </ul>
+                  qui seront déployés dans plus de 50 pays."
+              >
+                <ul>
+                  <li>
+                    Stack ReactJs :
+                    <ul>
+                      <li>SSR, Chunking, ...</li>
+                      <li>Redux, Webpack, Babel, Thunk, Axios, ...</li>
+                      <li>Jest, Enzyme, ...</li>
+                      <li>Eslint, Flow, ...</li>
+                    </ul>
+                  </li>
+                  <li>Gitlab, Gitlab-Ci, Docker de build</li>
+                  <li>
+                    Nginx, AEM, Controlleur en Java, Aggregateur en NodeJs
+                  </li>
+                  <li>Hébergement AWS : Cloudfront, Docker/ECS</li>
+                </ul>
               </Job>
             </div>
 
@@ -67,18 +70,35 @@ class App extends Component {
                 url="http://www.canalplus.fr/"
                 description="Chef de projet d&eacute;veloppement, d&eacute;veloppeur dans le cadre de la
                   refonte du SI Canal+ puis Lead Tech Full Stack
-                  sur la feature VENTE." >
-                  <ul>
-                    <li>Equipe Scrum (8 pers) : Interface Tablette Vendeur, Refonte des sites clients</li>
-                    <li>AngularJs, Node JS, Gulp, AWS, MongoDb, Jenkins, Puppet, Gitlab-CI</li>
-                    <li>Rôles :
-                      <ul>
-                        <li>Leader Technique de la feature VENTE (5 développeurs en mode full-stack)</li>
-                        <li>Mise en place et dev d'une application (FACE Tablet) pour les vendeurs en popup store en AngularJs 1.5.8</li>
-                        <li>Mise en place et dev de la refonte de espaceclient et parcours de vente web en AngularJs 1.6.0</li>
-                      </ul>
-                    </li>
-                  </ul>
+                  sur la feature VENTE."
+              >
+                <ul>
+                  <li>
+                    Equipe Scrum (8 pers) : Interface Tablette Vendeur, Refonte
+                    des sites clients
+                  </li>
+                  <li>
+                    AngularJs, Node JS, Gulp, AWS, MongoDb, Jenkins, Puppet,
+                    Gitlab-CI
+                  </li>
+                  <li>
+                    Rôles :
+                    <ul>
+                      <li>
+                        Leader Technique de la feature VENTE (5 développeurs en
+                        mode full-stack)
+                      </li>
+                      <li>
+                        Mise en place et dev d'une application (FACE Tablet)
+                        pour les vendeurs en popup store en AngularJs 1.5.8
+                      </li>
+                      <li>
+                        Mise en place et dev de la refonte de espaceclient et
+                        parcours de vente web en AngularJs 1.6.0
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </Job>
 
               <Job
@@ -88,21 +108,56 @@ class App extends Component {
                 url="http://www.bouyguestelecom.fr/"
                 description="Chef de projet d&eacute;veloppement et d&eacute;veloppeur sur
                   les sites Grand Public de Bouygues T&eacute;l&eacute;com.
-                " >
+                "
+              >
                 <ul>
-                  <li>Projets (6 à 15 pers) : Espace Client FAI, EC Unifié, Module Centralisé, Assistance Unifiée</li>
-                  <li>PHP5, SOAP, NodeJS, MYSQL5, NGINX, RethinkDb, Responsive, Adaptive, Jquery</li>
-                  <li>Rôles :
+                  <li>
+                    Projets (6 à 15 pers) : Espace Client FAI, EC Unifié, Module
+                    Centralisé, Assistance Unifiée
+                  </li>
+                  <li>
+                    PHP5, SOAP, NodeJS, MYSQL5, NGINX, RethinkDb, Responsive,
+                    Adaptive, Jquery
+                  </li>
+                  <li>
+                    Rôles :
                     <ul>
-                      <li>Développeur au sein des ex-équipes Club Internet, puis Référent technique et CP Dev.</li>
-                      <li>Projets web principalement sur le modèle "NGINX/PHP-FPM/WS SOAP Java/Bdd Oracle".</li>
-                      <li>Intégration au Club des experts de Bouygues Telecom (Rédaction d'articles internes)</li>
-                      <li>Diffusion de la philosophie de développement Fixe sur la partie Mobile de Bouygues Telecom.</li>
-                      <li>Outils de stats et d'analyse des sites BigData/Live (RabbitMQ, ElasticSearch, ...).</li>
-                      <li>Api REST en NodeJs qui permet d'intéragir avec une base RethinkDb.</li>
-                      <li>Header/footer responsive centralisé sur l'ensemble des sites Bouygues Telecom</li>
-                      <li>Assistance unifiée (GP, PRO, Desktop, Mobile) et Refontes des EC</li>
-                      <li>Finaliste du « Digital Hackaton » avec un sujet d'analyse prédictive de la navigation cliente</li>
+                      <li>
+                        Développeur au sein des ex-équipes Club Internet, puis
+                        Référent technique et CP Dev.
+                      </li>
+                      <li>
+                        Projets web principalement sur le modèle
+                        "NGINX/PHP-FPM/WS SOAP Java/Bdd Oracle".
+                      </li>
+                      <li>
+                        Intégration au Club des experts de Bouygues Telecom
+                        (Rédaction d'articles internes)
+                      </li>
+                      <li>
+                        Diffusion de la philosophie de développement Fixe sur la
+                        partie Mobile de Bouygues Telecom.
+                      </li>
+                      <li>
+                        Outils de stats et d'analyse des sites BigData/Live
+                        (RabbitMQ, ElasticSearch, ...).
+                      </li>
+                      <li>
+                        Api REST en NodeJs qui permet d'intéragir avec une base
+                        RethinkDb.
+                      </li>
+                      <li>
+                        Header/footer responsive centralisé sur l'ensemble des
+                        sites Bouygues Telecom
+                      </li>
+                      <li>
+                        Assistance unifiée (GP, PRO, Desktop, Mobile) et
+                        Refontes des EC
+                      </li>
+                      <li>
+                        Finaliste du « Digital Hackaton » avec un sujet
+                        d'analyse prédictive de la navigation cliente
+                      </li>
                     </ul>
                   </li>
                 </ul>
@@ -115,15 +170,33 @@ class App extends Component {
                 url="http://www.sopra.com/"
                 description="Les 5 ann&eacute;es chez Sopra Group m'ont permis d'acqu&eacute;rir
                   des bases solides dans le web et de pratiquer le suivi de projet chez de grands comptes.
-                " >
+                "
+              >
                 <ul>
-                  <li>Projet (5 pers) : Portail du département Hébergeur et Portail du département Production</li>
-                  <li>PHP5, MYSQL5, Apache, XHTML, CSS, Ajax, JSON, IIS, SQL SERVER, J2EE, Oracle, XHTML, Ajax, JSON, CSS, Unix, Korn Shell, Prototype, Tomcat</li>
-                  <li>Rôles :
+                  <li>
+                    Projet (5 pers) : Portail du département Hébergeur et
+                    Portail du département Production
+                  </li>
+                  <li>
+                    PHP5, MYSQL5, Apache, XHTML, CSS, Ajax, JSON, IIS, SQL
+                    SERVER, J2EE, Oracle, XHTML, Ajax, JSON, CSS, Unix, Korn
+                    Shell, Prototype, Tomcat
+                  </li>
+                  <li>
+                    Rôles :
                     <ul>
-                      <li>Management et développement d'une application Web des indicateurs de QOS.</li>
-                      <li>Développeur puis Responsable de l'équipe en charge de la TMA du portail de l'hébergeur</li>
-                      <li>Réalisation d'outils web pour faciliter le suivi projet des équipes de Sopra Group</li>
+                      <li>
+                        Management et développement d'une application Web des
+                        indicateurs de QOS.
+                      </li>
+                      <li>
+                        Développeur puis Responsable de l'équipe en charge de la
+                        TMA du portail de l'hébergeur
+                      </li>
+                      <li>
+                        Réalisation d'outils web pour faciliter le suivi projet
+                        des équipes de Sopra Group
+                      </li>
                     </ul>
                   </li>
                 </ul>
@@ -148,8 +221,6 @@ class App extends Component {
               <Title title="Contact" />
               <Contact />
             </div>
-
-
           </div>
         </div>
         <Footer />
