b04e54746df2986e9d7d5fb01fe6422c27d92802
Cédric Bazureau <cbazureau@gmail.com>
2019-07-13 15:22:37 +0200

    feat(webp): banner

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,18 +5,26 @@ import "./Header.css";
 
 const Header = ({ onSectionChange }) => (
   <header className="Header">
-    <Menu onSectionChange={onSectionChange} />
-    <div className="Header__banner">
-      <Image
-        className="Header__logo"
-        src="/static/img/identite.jpg"
-        alt="logo"
-        isLazyLoaded={false}
-      />
-      <h1 className="Header__name">C&eacute;dric Bazureau</h1>
-      <h2 className="Header__job" data-section="headline">
-        Lead Tech Front à Renault
-      </h2>
+    <Image
+      className="Header__background"
+      src="/static/img/banner.jpg"
+      alt="banner background"
+      isLazyLoaded={false}
+    />
+    <div className="Header__txt">
+      <Menu onSectionChange={onSectionChange} />
+      <div className="Header__banner">
+        <Image
+          className="Header__logo"
+          src="/static/img/identite.jpg"
+          alt="logo"
+          isLazyLoaded={false}
+        />
+        <h1 className="Header__name">C&eacute;dric Bazureau</h1>
+        <h2 className="Header__job" data-section="headline">
+          Lead Tech Front à Renault
+        </h2>
+      </div>
     </div>
   </header>
 );
