8d124bc1de5e1d6ab7458b7f7b34c66664699ecf
Cédric Bazureau <cbazureau@gmail.com>
2019-11-28 11:05:24 +0100

    feat(server-push): improve server-push

--- src/htaccess.js
+++ src/htaccess.js
@@ -1,20 +1,34 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const assets = JSON.parse(
-  fs.readFileSync("./build/asset-manifest.json", "utf8")
-);
+// This script is reading the asset-manifest.json in order to create the Link Header
+// to server push js/css ressources
+
+const assets = JSON.parse(fs.readFileSync('./build/asset-manifest.json', 'utf8'));
 
 const fileTransform = (file, transformation) => {
-  fs.readFile(file, "utf8", (err, data) => {
+  fs.readFile(file, 'utf8', (err, data) => {
     if (err) return console.log(err);
-    fs.writeFile(file, transformation(data), "utf8", err => {
+    fs.writeFile(file, transformation(data), 'utf8', err => {
       if (err) return console.log(err);
     });
   });
 };
 
-fileTransform("./build/.htaccess", data => {
-  return data
-    .replace("</main.css>", `<${assets.files["main.css"]}>`)
-    .replace("</main.js>", `<${assets.files["main.js"]}>`);
+// Build Header Link
+// </main.css>; rel=preload; as=style,</main.js>; rel=preload; as=script
+fileTransform('./build/.htaccess', data => {
+  const headerLink = Object.keys(assets.files)
+    .filter(k => {
+      return (
+        (k.endsWith('.css') || k.endsWith('.js')) && (k.startsWith('main') || k.includes('chunk'))
+      );
+    })
+    .map(k => {
+      const file = assets.files[k];
+      if (k.endsWith('.css')) return `<${file}>; rel=preload; as=style`;
+      else if (k.endsWith('.js')) return `<${file}>; rel=preload; as=script`;
+      else return null;
+    })
+    .join(', ');
+  return data.replace('HEADER-LINK', headerLink);
 });
